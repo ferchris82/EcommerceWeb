1d0c9b7fb2f3576956fdb30c75420a4b30812dbc
ChrisferDev <ferchris82@hotmail.com>
2024-08-09 13:33:11 -0500

    Versión 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     DemoAngularMaterialModule
   ],
   providers: [
